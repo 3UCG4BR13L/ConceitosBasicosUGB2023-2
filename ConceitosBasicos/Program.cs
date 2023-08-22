b740d5b5c7dda40a1104b5e8ce77d0214214eb88
2021101146 <2021101146@LAB2-004>
2023-08-22 18:55:11 -0400

    Adicionar arquivos de projeto.

---
+++ ConceitosBasicos/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Console.WriteLine("Hello, World!");
+
+int num1 = 20;
+int num2 = 22;
+int soma = num1 + num2;
+Console.WriteLine($"{num1} + {num2} = {soma}");
+
+
+float pi = 3.141597f;
+
+decimal salario = 1700000000.00m;
+
+decimal valorCompra = 17.04m;
+decimal valorVenda = valorCompra++ + 10;
+Console.WriteLine($"Valor de Venda: {valorVenda}");
+Console.WriteLine($"Valor de Compra: {--valorCompra}");
+
+double pi  = Math.PI;
+Console.WriteLine("PI: {0:}", pi);
+*/
+bool foiPromovido = true;
+
+if (foiPromovido)
+{
+    double salario = 4200.0;
+}
+else
+{
+    double salario = 3800.0;
+}
+Console.WriteLine(salario);
